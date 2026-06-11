c5b00a4194b05f3e1b76b6699ecc2518f21d09ba
Pratham Sahasrabudhe <superchessbeginner@gmail.com>
2026-06-11 16:23:05 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map <char,int> mpp1;
+        map <char,int> mpp2;
+        if(s.size() != t.size())
+        return false;
+        for(int i = 0; i<s.size(); i++)
+        {
+            mpp1[s[i]]++;
+        }
+         for(int i = 0; i<t.size(); i++)
+        {
+            mpp2[t[i]]++;
+        }
+        if(mpp1 == mpp2)
+        return true;
+        return false;
+        
+    }
+};
